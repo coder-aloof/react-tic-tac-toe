3785433773cb8589c324cc80de2ef5300bd5ce9f
Nirmal Kumar <codingboredom@gmail.com>
2020-03-07 00:24:34 +0530

    Updates

---
+++ precache-manifest.1df8641334e793108fd57084b2b8243d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79beec4b7c26745aca44b61c8c1e2787",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "dbed9d2bf20578cf5445",
+    "url": "/react-tic-tac-toe/static/css/main.c178ec92.chunk.css"
+  },
+  {
+    "revision": "45a78fae37a31c1d37d7",
+    "url": "/react-tic-tac-toe/static/js/2.205f908e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tic-tac-toe/static/js/2.205f908e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbed9d2bf20578cf5445",
+    "url": "/react-tic-tac-toe/static/js/main.99bc4e5c.chunk.js"
+  },
+  {
+    "revision": "2eab5db79a23bb3f034d",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.87d4507d.js"
+  }
+]);
